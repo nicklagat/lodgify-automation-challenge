fb4a1438aeca34ff92b9efa670ef73b1d6b3948a
nicklagat <nicksonlagatk@gmail.com>
2023-05-21 22:00:02 +0300

    Update Cypress configuration with video recording, environment variables, browser selection, and reporter settings

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
+  videoTimeout: 120000,
+  videoCompression: 5,
   e2e: {
+    "baseUrl": "https://todoist.com",
+    "env": {
+      "apiBaseUrl": "https://api.todoist.com",
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  
+  browser: "chrome",
+
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: false,
   },
 });
