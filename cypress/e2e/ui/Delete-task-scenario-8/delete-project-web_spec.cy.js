124543aef7f4bb85cfb371559f26af6e73cc3b4f
nicklagat <nicksonlagatk@gmail.com>
2023-05-26 01:12:28 +0300

    Updated delete project via web update

--- cypress/e2e/ui/Delete-task-scenario-8/delete-project-web_spec.cy.js
+++ cypress/e2e/ui/Delete-task-scenario-8/delete-project-web_spec.cy.js
@@ -15,7 +15,7 @@ describe("Delete Project via Web Application", () => {
 
     cy.wait(Math.floor(Math.random() * (60000 - 30000 + 1)) + 30000);
 
-    const projectPage = new DeleteWebPage();
+    const projectPage = new DeleteWebPage();l 
     projectPage
       .addProject(projectName)
       .selectProject(projectName)
@@ -23,36 +23,3 @@ describe("Delete Project via Web Application", () => {
   });
 });
 
-// cy.wait(Math.floor(Math.random() * (60000 - 30000 + 1)) + 30000);
-
-// cy.get("button")
-//   .filter(
-//     '[aria-label="Add project"][type="button"][aria-disabled="false"][tabindex="0"]'
-//   )
-//   .click()
-//   .then(() => {
-//     // Here you can interact with the modal or perform further actions
-//     // Creating a project via UI
-//     cy.get('[data-testid="modal-overlay"]')
-//       .find("input#edit_project_modal_field_name")
-//       .type(projectName);
-
-//     cy.get("button")
-//       .filter('[type="submit"][aria-disabled="false"][class^="_8313bd46"]')
-//       .click();
-//   });
-
-// cy.wait(15000);
-// cy.get("#projects_list li").contains(projectName).click();
-// cy.get('button[aria-label="More project actions"][aria-expanded="false"]')
-//   .last()
-//   .click();
-
-// cy.contains("div.icon_menu_item__content", "Delete project").click({
-//   force: true,
-// });
-
-// cy.get('[data-testid="modal-overlay"]');
-// cy.get("[data-dialog]")
-//   .find('button[data-autofocus="true"][type="submit"]')
-//   .click();
